f0ba23fb5f2bfa0329f3a72a2edc138805ed6f1b
BoHuny <BoHuny@hotmail.com>
2019-09-07 20:01:32 -0400

    Ajout corps de classe modeles

---
+++ app/Personne.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Personne extends Model
+{
+    protected $table = 'personne'; /*Definit le nom de la table de la BD correspondant a associer au modele
+                                        (par defaut la valeur de $table est le nom de la classe en snake case suivit d'un s)*/
+
+
+}

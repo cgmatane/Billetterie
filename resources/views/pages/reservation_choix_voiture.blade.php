553c6e5bcd3b93b6a3e588325a89b26378405c26
BoHuny <BoHuny@hotmail.com>
2019-09-12 00:11:30 -0400

    Configuration du css et ajout des fichiers vues pour chacune des pages

---
+++ resources/views/pages/reservation_choix_voiture.blade.php
@@ -0,0 +1,26 @@
+<body id="top" class="" style="background-color: #004882">
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h4 class="text-center text-warning p-4 font-weight-bold">Votre véhicule est une voiture ?</h4>
+        </div>
+    </div>
+    <div class="row text-center">
+        <a href="index.php?page=reservation_choix_remorque" class="bg-white rounded-sm col-5 ml-1 mx-auto pb-5 pt-5 display-4"
+           style="height: 200px; text-decoration:none;">OUI</a>
+
+        <a href="index.php?page=reservation_choix_autre_vehicule" class="bg-white rounded-sm col-5 ml-1 mx-auto pb-5 pt-5 display-4"
+           style="height: 200px; text-decoration:none;">NON</a>
+    </div>
+
+    <div class="row text-center justify-content-center">
+        <a href="index.php?page=reservation_choix_vehicule"><button type="button" class="btn btn-warning mt-5 mb-5 px-5 py-lg-3 py-xs-2">
+                Retour au choix precedent </button></a>
+    </div>
+
+    <div class="row text-center justify-content-center">
+        <a href="index.php?page=accueil"><button type="button" class="btn btn-danger px-5 py-lg-3 py-xs-2">
+                Recommencer</button></a>
+    </div>
+</div>
+

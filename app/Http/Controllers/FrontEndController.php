dd0a321eacb3c7afe775f8d285fd8906912291ab
BoHuny <BoHuny@hotmail.com>
2019-09-15 12:59:35 -0400

    Ajout d'une fonction du controller pour chaque page (solution temporaire avant factorisation)

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -10,5 +10,63 @@ class FrontEndController extends Controller
         return view('pages.accueil');
     }
 
+    public function choixDate() {
+        return view('pages.choix_date');
+    }
 
+    public function choixDepart() {
+        return view('pages.choix_depart');
+    }
+
+    public function informations() {
+        return view('pages.informations');
+    }
+
+    public function informationsAnimaux() {
+        return view('pages.informations_animaux');
+    }
+
+    public function informationsMatieres() {
+        return view('pages.informations_matieres');
+    }
+
+    public function reservationChoixAutreVehicule() {
+        return view('pages.reservation_choix_autre_vehicule');
+    }
+
+    public function reservationChoixHoraireDepart() {
+        return view('pages.reservation_choix_horaire_depart');
+    }
+
+    public function reservationChoixRemorque() {
+        return view('pages.reservation_choix_remorque');
+    }
+
+    public function reservationChoixVehicule() {
+        return view('pages.reservation_choix_vehicule');
+    }
+
+    public function reservationChoixVoiture() {
+        return view('pages.reservation_choix_voiture');
+    }
+
+    public function reservationConfirmation() {
+        return view('pages.reservation_confirmation');
+    }
+
+    public function reservationMatieres() {
+        return view('pages.reservation_matieres');
+    }
+
+    public function reservationPaiement() {
+        return view('pages.reservation_paiement');
+    }
+
+    public function reservationPassagers() {
+        return view('pages.reservation_passagers');
+    }
+
+    public function reservationPoids() {
+        return view('pages.reservation_poids');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,64 +11,34 @@
 |
 */
 
-Route::get('/choix/date', function () {
-    return view('pages.choix_date');
-})->name('choix_date');
+Route::get('/choix/date', 'FrontEndController@choixDate')->name('choix_date');
 
-Route::get('/choix/depart', function () {
-    return view('pages.choix_depart');
-})->name('choix_depart');
+Route::get('/choix/depart', 'FrontEndController@choixDepart')->name('choix_depart');
 
-Route::get('/informations', function () {
-    return view('pages.informations');
-})->name('informations');
+Route::get('/informations', 'FrontEndController@informations')->name('informations');
 
-Route::get('/informations/animaux', function () {
-    return view('pages.informations_animaux');
-})->name('informations_animaux');
+Route::get('/informations/animaux', 'FrontEndController@informationsAnimaux')->name('informations_animaux');
 
-Route::get('/informations/matieres', function () {
-    return view('pages.informations_matieres');
-})->name('informations_matieres');
+Route::get('/informations/matieres', 'FrontEndController@informationsMatieres')->name('informations_matieres');
 
-Route::get('/choix/autre_vehicule', function () {
-    return view('pages.reservation_choix_autre_vehicule');
-})->name('reservation_choix_autre_vehicule');
+Route::get('/choix/autre_vehicule', 'FrontEndController@reservationChoixAutreVehicule')->name('reservation_choix_autre_vehicule');
 
-Route::get('/choix/horaires_depart', function () {
-    return view('pages.reservation_choix_horaire_depart');
-})->name('reservation_choix_horaire_depart');
+Route::get('/choix/horaires_depart', 'FrontEndController@reservationChoixHoraireDepart')->name('reservation_choix_horaire_depart');
 
-Route::get('/choix/remorque', function () {
-    return view('pages.reservation_choix_remorque');
-})->name('reservation_choix_remorque');
+Route::get('/choix/remorque', 'FrontEndController@reservationChoixRemorque')->name('reservation_choix_remorque');
 
-Route::get('/choix/vehicule', function () {
-    return view('pages.reservation_choix_vehicule');
-})->name('reservation_choix_vehicule');
+Route::get('/choix/vehicule', 'FrontEndController@reservationChoixVehicule')->name('reservation_choix_vehicule');
 
-Route::get('/choix/voiture', function () {
-    return view('pages.reservation_choix_voiture');
-})->name('reservation_choix_voiture');
+Route::get('/choix/voiture', 'FrontEndController@reservationChoixVoiture')->name('reservation_choix_voiture');
 
-Route::get('/reservation/confirmation', function () {
-    return view('pages.reservation_confirmation');
-})->name('reservation_confirmation');
+Route::get('/reservation/confirmation', 'FrontEndController@reservationConfirmation')->name('reservation_confirmation');
 
-Route::get('/choix/matieres_dangereuses', function () {
-    return view('pages.reservation_matieres');
-})->name('reservation_matieres');
+Route::get('/choix/matieres_dangereuses', 'FrontEndController@reservationMatieres')->name('reservation_matieres');
 
-Route::get('/reservation/paiement', function () {
-    return view('pages.reservation_paiement');
-})->name('reservation_paiement');
+Route::get('/reservation/paiement', 'FrontEndController@reservationPaiement')->name('reservation_paiement');
 
-Route::get('/reservation/passagers', function () {
-    return view('pages.reservation_passagers');
-})->name('reservation_passagers');
+Route::get('/reservation/passagers', 'FrontEndController@reservationPassagers')->name('reservation_passagers');
 
-Route::get('/choix/poids_vehicule', function () {
-    return view('pages.reservation_poids');
-})->name('reservation_poids');
+Route::get('/choix/poids_vehicule', 'FrontEndController@reservationPoids')->name('reservation_poids');
 
 Route::get('/', 'FrontEndController@accueil')->name('index');

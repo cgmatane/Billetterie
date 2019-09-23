e74b8bc2e526ea3c0bd08b5d6a4f651d8f7e89c5
Loic Hug <h-loic@orange.fr>
2019-09-22 21:33:51 -0400

    modification bouton choix destination

--- resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
+++ resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
@@ -1,5 +1,7 @@
 <a href="{{ route('reservation_choix_vehicule') }}?destination={{$destination}}&heure={{$heure}}">
-    <button type="button" class="btn btn-success mt-5 col-6 p-3" >
-        {{ $destination }} : {{ $heure }}
+    <button type="button" class="btn btn-outline-success mt-3 col-11 p-3" >
+        <div class="display-4">
+            <p> destination {{ $destination }} départ à {{ $heure }}</p>
+        </div>
     </button>
 </a>

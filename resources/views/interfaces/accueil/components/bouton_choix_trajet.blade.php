6ba2a1b2c4f193508c5619edf4823dc23ef3329c
Loic Hug <h-loic@orange.fr>
2019-09-22 21:43:36 -0400

    modification taille police accueil

--- resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
+++ resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('reservation_choix_vehicule') }}?destination={{$destination}}&heure={{$heure}}">
-    <button type="button" class="btn btn-outline-success mt-3 col-11 p-3" >
+    <button type="button" class="btn btn-outline-success mt-3 col-11 pt-3" >
         <div class="display-4">
             <p> destination {{ $destination }} départ à {{ $heure }}</p>
         </div>

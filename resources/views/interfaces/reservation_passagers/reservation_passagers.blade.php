1ee69a7bfb44a659c97dbe0f3590bacd2c6924c4
Loic Hug <h-loic@orange.fr>
2019-09-24 21:16:21 -0400

    ajout formulaire informations passagers

--- resources/views/interfaces/reservation_passagers/reservation_passagers.blade.php
+++ resources/views/interfaces/reservation_passagers/reservation_passagers.blade.php
@@ -16,65 +16,53 @@
         <div class = "col">Surplus charge lourde : {{$poids_eleve}}</div>
     </div> -->
     <div class="row">
-        <div class="col-12">
-            <h4 class="text-center text-info font-weight-bold mt-4">Renseignez vos informations</h4>
-        </div>
     </div>
-    <div class="row text-center justify-content-center mb-5 mt-5">
-    <form>
-        <div class="row">
-            <div class="col">
-                <label for="nom">Nom :</label>
-            </div>
-            <div class="col-sm-7">
-                <input type="text" class="form-control" id="nom">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <label for="prenom">Prénom :</label>
-            </div>
-            <div class="col-sm-7">
-                <input type="text" class="form-control" id="prenom">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <label for="exampleFormControlInput1">Courriel :</label>
-            </div>
-            <div class="col-sm-7">
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <label for="phone">Numéro de mobile :</label>
-            </div>
-            <div class="col-sm-7">
-                <input type="tel" class="form-control" id="phone" placeholder="418 000-1234">
-            </div>
-        </div>
-        <div class="row">
-                <div>
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Je ne voyage pas avec des animaux exotiques</label>
-                    <br/>
-                    <input type="checkbox" class="form-check-input" id="exampleCheck2">
-                    <label class="form-check-label" for="exampleCheck2">Je ne transporte pas de marchandises dangereuses</label>
-                </div>
-        </div>
-    </form>
-    </div>
+        <!-- Default form register -->
+        <form class="text-center border border-light p-5 mt-5 mb-5">
 
-    <div class="row text-center justify-content-center">
-        <a href="{{ route('reservation_paiement') }}"><button type="button" class="btn btn-success mx-5 my-2 px-5 py-lg-5 py-xs-2">
-                Procéder au paiement</button></a>
-    </div>
+            <p class="h2 mb-4 text-info font-weight-bold mb-5">Renseignez vos informations</p>
+
+            <div class="form-row mb-4">
+                <div class="col">
+                    <!-- First name -->
+                    <input type="text" id="defaultRegisterFormFirstName" class="form-control mb-3" placeholder="Nom">
+                </div>
+                <div class="col">
+                    <!-- Last name -->
+                    <input type="text" id="defaultRegisterFormLastName" class="form-control mb-3" placeholder="Prenom">
+                </div>
+            </div>
+
+            <!-- E-mail -->
+            <input type="email" id="defaultRegisterFormEmail" class="form-control mb-4 mb-3" placeholder="E-mail">
+
+            <!-- Phone number -->
+            <input type="text" id="defaultRegisterPhonePassword" class="form-control mb-3" placeholder="Numéro de Téléphone" aria-describedby="defaultRegisterFormPhoneHelpBlock">
+            <small id="defaultRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
+                nécessaire en cas d'annulation ou de retard
+            </small>
+
+            <!-- Newsletter -->
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="defaultRegisterFormNewsletter">
+                <label class="custom-control-label" for="defaultRegisterFormNewsletter">je confirme ne pas transporter de matières dangereuses</label>
+            </div>
+
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="defaultRegisterFormNewsletter">
+                <label class="custom-control-label" for="defaultRegisterFormNewsletter">je confirme ne pas transporter d'animaux exotiques</label>
+            </div>
+
+            <!-- Sign up button -->
+            <a href="{{ route('reservation_paiement') }}"><button type="button" class="btn btn-outline-success my-4 btn-block">
+                    Procéder au paiement</button></a>
+
+        </form>
 </div>
 <div class="container-fluid">
         <div style="width: 100% ;height: 400px; background-color: #002A4D; margin-top: -50px; border-radius: 20px">
             <div class="row">
-                <div style="float: left; margin-left: 5%; margin-top: 15%">
+                <div style="float: left; margin-left: 5%; margin-top: 55px">
                     @component('global_components.bouton_retour_precedent')
                         {{ $global_retour_choix_precedent }}
                     @endcomponent

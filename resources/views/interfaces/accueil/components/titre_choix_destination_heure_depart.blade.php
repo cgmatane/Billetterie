f43a7b1e031f2693054464e4b5b25c405d74e0aa
Loic Hug <h-loic@orange.fr>
2019-09-25 15:00:28 -0400

    modification titre date de depart

--- resources/views/interfaces/accueil/components/titre_choix_destination_heure_depart.blade.php
+++ resources/views/interfaces/accueil/components/titre_choix_destination_heure_depart.blade.php
@@ -1,1 +1,1 @@
-<h3 class="text-center mt-3">{{ $slot }}</h3>
+<h3 class="text-center mt-4">{{ $slot }}</h3>
--- resources/views/interfaces/accueil/components/titre_principal.blade.php
+++ resources/views/interfaces/accueil/components/titre_principal.blade.php
@@ -1,1 +1,1 @@
-<h1 class="text-center text-warning font-weight-bold">{{ $slot }}</h1>
+<h1 class="text-center font-weight-bold" style="color: white">{{ $slot }}</h1>

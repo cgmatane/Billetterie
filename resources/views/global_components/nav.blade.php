a22717be345b7222f558af5b9f2d09476f3a56c5
Antoine68 <antoine.richard68@gmail.com>
2019-09-28 18:09:27 -0400

    Bouton dropdown pour choisir dans le nav connexion/inscription

--- resources/views/global_components/nav.blade.php
+++ resources/views/global_components/nav.blade.php
@@ -12,7 +12,17 @@
                 </div>
                 <div class="col-sm-4 col-xs-12 d-flex justify-content-end align-items-center text-dark ">
                     <a class="mx-auto btn btn-lg btn-outline-primary" href="{{ route('informations') }}">Informations</a> <!-- Route à définir -->
-                    <a class="mx-auto btn btn-lg btn-outline-primary" href="{{ route('connexion') }}">Se connecter</a> <!-- Route à définir -->
+
+                    <div class="btn-group">
+                        <a class="mx-auto btn btn-lg btn-outline-primary" href="{{ route('connexion') }}">Mon compte</a>
+                        <button type="button" class="mx-auto btn btn-lg btn-outline-primary dropdown-toggle" style="border-left: none;" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="{{ route('connexion') }}">Se connecter</a></li>
+                            <li><a class="dropdown-item" href="#">S'inscrire</a></li>
+                        </ul>
+                    </div>
+                    
                 </div>
             </div>
         </header>

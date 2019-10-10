4aa91d3d97bfebd305cf81f9862643531daa993d
BoHuny <BoHuny@hotmail.com>
2019-10-10 14:35:27 -0400

    Ajout gestion messages d'erreur formulaire passagers

---
+++ public/js/reservation_passagers.js
@@ -0,0 +1,139 @@
+    let increment;
+    let passagerVitrine;
+    let divPassagers;
+    let boutonMoins;
+    let separateur = '-';
+
+    document.addEventListener("DOMContentLoaded", onLoad);
+
+
+    function onLoad() {
+        increment = 0;
+        passagerVitrine = document.getElementById('passager');
+        divPassagers = document.getElementById('passagers');
+        boutonMoins = document.getElementById('boutonRetirerPassager');
+        ajouterPassager();
+    }
+
+    function ajouterPassager() {
+        increment++;
+        if (increment === 2) {
+            boutonMoins.style.display = "inline-block";
+        }
+        let clonePassager = passagerVitrine.cloneNode(true);
+        clonePassager.id += separateur+increment;
+        clonePassager.getElementsByClassName('legendePassager')[0].innerHTML += " "+increment;
+        clonePassager.getElementsByClassName('champNom')[0].id += separateur + increment;
+        clonePassager.getElementsByClassName('champNom')[0].getElementsByTagName('input')[0].name = "nom[]";
+        clonePassager.getElementsByClassName('champPrenom')[0].id += separateur + increment;
+        clonePassager.getElementsByClassName('champPrenom')[0].getElementsByTagName('input')[0].name = "prenom[]";
+        clonePassager.getElementsByClassName('champAge')[0].id += separateur + increment;
+        clonePassager.getElementsByClassName('champAge')[0].getElementsByTagName('select')[0].name = "age[]";
+        clonePassager.style.display = "block";
+
+        divPassagers.appendChild(clonePassager);
+
+    }
+
+    function supprimerPassager() {
+        //Ne peux pas supprimer un passager s'il n'y en a qu'un
+        if (increment === 1)
+            return;
+        let dernierPassager = document.getElementById('passager'+separateur+increment);
+        divPassagers.removeChild(dernierPassager);
+        increment--;
+        if(increment === 1) {
+            boutonMoins.style.display = "none";
+        }
+    }
+
+    function validerFormulaire() {
+        let erreur = false;
+        let passagers = document.getElementsByClassName("passager");
+        let champ;
+        //On commence a 1 pour skip le passager vitrine
+        for (let i = 1;i<passagers.length;i++) {
+            champ = passagers[i].getElementsByClassName('champNom')[0];
+            if (!estChampValide(champ)) {
+                erreur = true;
+                setMessageErreurChamp(champ, 'Veuillez saisir un nom');
+            }
+            else {
+                setMessageErreurChamp(champ);
+            }
+            champ = passagers[i].getElementsByClassName('champPrenom')[0];
+            if (!estChampValide(champ)) {
+                erreur = true;
+                setMessageErreurChamp(champ, 'Veuillez saisir un prénom');
+            }
+            else {
+                setMessageErreurChamp(champ);
+            }
+        }
+        champ = document.getElementById('champCourriel');
+        if (!estChampValide(champ)) {
+            erreur = true;
+            setMessageErreurChamp(champ, 'Veuillez saisir un courriel');
+        }
+        else {
+            setMessageErreurChamp(champ);
+        }
+        champ = document.getElementById('champTelephone');
+        if (!estChampValide(champ)) {
+            erreur = true;
+            setMessageErreurChamp(champ, 'Veuillez saisir un numéro de téléphone');
+        }
+        else {
+            setMessageErreurChamp(champ);
+        }
+        champ = document.getElementById('checkboxMatieres');
+        if (!estChampValide(champ)) {
+            erreur = true;
+            setMessageErreurChamp(champ, 'Vous devez confirmer que vous ne transportez pas de matériaux dangereux pour continuer');
+        }
+        else {
+            setMessageErreurChamp(champ);
+        }
+        champ = document.getElementById('checkboxAnimaux');
+        if (!estChampValide(champ)) {
+            erreur = true;
+            setMessageErreurChamp(champ, 'Vous devez confirmer que vous ne voyagez pas avec des animaux exotiques pour continuer');
+        }
+        else {
+            setMessageErreurChamp(champ);
+        }
+
+        return !erreur;
+    }
+
+    function estChampValide(champ) {
+        input = champ.getElementsByTagName('input')[0];
+        if (input.type === "checkbox") {
+            return input.checked;
+        }
+        valide = input.value.length > 0;
+        surlignerInput(input, !valide);
+        return valide;
+    }
+
+    function surlignerInput(champ, erreur)
+    {
+        let couleur;
+        if (erreur) {
+            couleur = '#fba';
+        }
+        else {
+            couleur = 'transparent';
+        }
+        champ.style.borderColor = couleur;
+    }
+
+    function setMessageErreurChamp(champ, messageErreur = null) {
+        if (messageErreur == null) {
+            champ.getElementsByClassName('champErreur')[0].style.display = "none";
+            champ.getElementsByClassName('texteErreur')[0].innerHTML = '';
+            return;
+        }
+        champ.getElementsByClassName('champErreur')[0].style.display = "block";
+        champ.getElementsByClassName('texteErreur')[0].innerHTML = messageErreur;
+    }
--- resources/views/interfaces/reservation_passagers/reservation_passagers.blade.php
+++ resources/views/interfaces/reservation_passagers/reservation_passagers.blade.php
@@ -6,82 +6,133 @@
     <div class="container">
 
 
-        <form class="text-center border border-light p-5 mt-5 mb-5" method="get" action="{{ route('reservation_passagers') }}"
+        <form class="text-center border border-light p-5 mt-5 mb-5" method="get"
+              action="{{ route('reservation_passagers') }}"
               onsubmit="return validerFormulaire()">
-            {{ csrf_field() }}
-            <p class="h2 mb-4 font-weight-bold mb-5"
-               style="color: midnightblue">{{ $reservation_passagers_renseigner_informations }}</p>
 
-            <span class="mb-2 mt-2" id="passagers">
-    <fieldset class="form-group passager" id="passager" style="border: #1b1e21 dashed thin;display: none;">
-        <div class="row p-0 m-2">
-            <legend class="legendePassager">Passager</legend>
-            <input type="text" class="form-control champNom col-sm-5" id="champNom"
-                   style="border-width:medium" placeholder="{{ $reservation_passagers_nom }}">
-            <input type="text" class="form-control champPrenom col-sm-5" id="champPrenom"
-                   style="border-width:medium" placeholder="{{ $reservation_passagers_prenom }}">
-            <select class="browser-default custom-select champAge col-sm-2" id="champAge">
-                @foreach($reservation_passagers_intervalles_age as $cle => $intervalle_age)
-                    <option value="{{ $intervalle_age }}"
-                            @if ($cle == $reservation_passagers_index_intervalle_age_par_defaut)
-                            selected
-                    @endif
-                    >{{ $intervalle_age }}</option>
-                @endforeach
-            </select>
-        </div>
-    </fieldset>
-</span>
+            <p class="h2 mb-4 font-weight-bold mb-5" style="color: midnightblue">
+                {{ $reservation_passagers_renseigner_informations }}
+            </p>
 
-            <button id="bouton-plus" type="button" class="btn btn-success" style="display: inline-block;" onclick="ajouterPassager();">
-                {{ $reservation_passagers_ajouter_passager }}
-            </button>
-            <button id="bouton-moins" type="button" class="btn btn-danger" style="display: none;" onclick="supprimerPassager();">
-                {{ $reservation_passagers_retirer_passager }}
-            </button>
+            <span id="passagers">
+                <fieldset class="passager mb-2" id="passager" style="border: #1b1e21 dashed thin;display:none;">
+                    <div class="legendePassager">Passager</div>
+                    <div class="row p-0 m-2">
+                        <div class="col-sm px-0">
+                            <div class="form-group champNom" id="champNom">
+                                <input type="text" class="form-control" style="border-width:medium"
+                                       placeholder="{{ $reservation_passagers_nom }}">
+                                <div class="alert alert-danger champErreur" style="display:none">
+                                    <small class="texteErreur"></small>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm px-0">
+                            <div class="form-group champPrenom" id="champPrenom">
+                                <input type="text" class="form-control"
+                                       style="border-width:medium" placeholder="{{ $reservation_passagers_prenom }}">
+                                <div class="alert alert-danger champErreur" style="display:none">
+                                    <small class="texteErreur"></small>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 px-0">
+                            <div class="form-group champAge" id="champAge">
+                                <select class="browser-default custom-select">
+                                    @foreach($reservation_passagers_intervalles_age as $cle => $intervalle_age)
+                                        <option value="{{ $intervalle_age }}"
+                                            @if ($cle == $reservation_passagers_index_intervalle_age_par_defaut)
+                                                selected
+                                            @endif
+                                        >
+                                            {{ $intervalle_age }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
+            </span>
 
-            <fieldset class="form-group mt-3">
+            <div class="text-right">
+                <button type="button" id="boutonAjouterPassager" class="btn btn-success"
+                        onclick="ajouterPassager();">
+                    {{ $reservation_passagers_ajouter_passager }}
+                </button>
+                <button type="button" id="boutonRetirerPassager" class="btn btn-danger pull-right" style="display:none"
+                        onclick="supprimerPassager();">
+                    {{ $reservation_passagers_retirer_passager }}
+                </button>
+            </div>
 
-                <input name="email" type="email" class="form-control m-2" id="champCourriel" style="border-width:medium"
-                       placeholder="{{ $reservation_passagers_courriel }}">
 
-                <input name="numero" type="tel" class="form-control m-2" id="champTelephone" style="border-width:medium"
-                       placeholder="{{ $reservation_passagers_numero }}"
-                       aria-describedby="defaultRegisterFormPhoneHelpBlock">
-                <small id="defaultRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
-                    {{ $reservation_passagers_tel_necessaire }}
-                </small>
+            <fieldset class="mt-3">
+                <div class="row">
+                    <div class="form-group col" id="champCourriel">
+                        <input name="email" type="email" class="form-control"
+                               style="border-width:medium"
+                               placeholder="{{ $reservation_passagers_courriel }}">
+                        <div class="alert alert-danger champErreur" style="display:none">
+                            <small class="texteErreur"></small>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col" id="champTelephone">
+                        <input name="numero" type="tel" class="form-control"
+                               style="border-width:medium"
+                               placeholder="{{ $reservation_passagers_numero }}"
+                               aria-describedby="defaultRegisterFormPhoneHelpBlock">
+                        <div class="alert alert-danger champErreur" style="display:none">
+                            <small class="texteErreur"></small>
+                        </div>
+                        <small class="form-text text-muted mb-4">
+                            {{ $reservation_passagers_tel_necessaire }}
+                        </small>
+                    </div>
+                </div>
 
             </fieldset>
-
-            <div class="form-group">
-                <input type="checkbox" id="checkbox" style="display: none;">
-                <label for="checkbox" class="check">
-                    <svg width="18px" height="18px" viewBox="0 0 18 18">
-                        <path
-                            d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
-                        <polyline points="1 9 7 14 15 4"></polyline>
-                    </svg>
-                </label>
-                <label class="form-check-label" for="checkbox">{{$reservation_passagers_confirmation_matieres }}</label>
+            <div class="row">
+                <div class="form-group col" id="checkboxMatieres">
+                    <input type="checkbox" id="checkbox" style="display: none;">
+                    <label for="checkbox" class="check">
+                        <svg width="18px" height="18px" viewBox="0 0 18 18">
+                            <path
+                                d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                            <polyline points="1 9 7 14 15 4"></polyline>
+                        </svg>
+                    </label>
+                    <label class="form-check-label" for="checkbox">
+                        {{$reservation_passagers_confirmation_matieres }}
+                    </label>
+                    <div class="alert alert-danger champErreur" style="display:none">
+                        <small class="texteErreur"></small>
+                    </div>
+                </div>
             </div>
-
-            <div class="form-group">
-                <input type="checkbox" id="checkbox2" style="display: none;">
-                <label for="checkbox2" class="check">
-                    <svg width="18px" height="18px" viewBox="0 0 18 18">
-                        <path
-                            d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
-                        <polyline points="1 9 7 14 15 4"></polyline>
-                    </svg>
-                </label>
-                <label class="form-check-label" for="checkbox">{{$reservation_passagers_confirmation_animaux }}</label>
+            <div class="row">
+                <div class="form-group col" id="checkboxAnimaux">
+                    <input type="checkbox" id="checkbox2" style="display: none;">
+                    <label for="checkbox2" class="check">
+                        <svg width="18px" height="18px" viewBox="0 0 18 18">
+                            <path
+                                d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                            <polyline points="1 9 7 14 15 4"></polyline>
+                        </svg>
+                    </label>
+                    <label class="form-check-label" for="checkbox2">
+                        {{$reservation_passagers_confirmation_animaux }}
+                    </label>
+                    <div class="alert alert-danger champErreur" style="display:none">
+                        <small class="texteErreur"></small>
+                    </div>
+                </div>
             </div>
-
             <button type="submit" class="btn btn-outline-success my-4 btn-block">
                 {{ $reservation_passagers_paiement}}
             </button>
-
         </form>
 
     </div>
@@ -108,5 +159,5 @@
     @endsection
 
     @section('scripts')
-        <script type="text/javascript" src="{{URL::asset('js/ajout_passagers.js')}}"></script>
-    @endsection
+        <script type="text/javascript" src="{{URL::asset('js/reservation_passagers.js')}}"></script>
+@endsection

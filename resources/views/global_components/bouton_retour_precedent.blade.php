c471f50a57a4d88a34dba5493d67a0887886e5e5
Niruban Thuraisingham <1531436@cgmatane.qc.ca>
2019-10-17 15:20:56 -0400

    icone page d'acceuille

--- resources/views/global_components/bouton_retour_precedent.blade.php
+++ resources/views/global_components/bouton_retour_precedent.blade.php
@@ -1,5 +1,5 @@
 <a href="javascript:history.go(-1)">
-    <button type="button" class="btn btn-outline-retour p-3">
+    <button type="button" class="btn btn-outline-retour p-3"> <i class="fas fa-arrow-circle-left"></i>
         {{ $slot }}
     </button>
 </a>

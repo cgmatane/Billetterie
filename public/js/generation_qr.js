0903201e3912d10873bf125b93c8b882b8ebe085
Raphaël Lavoie <rapolysec@hotmail.com>
2019-10-17 15:54:37 -0400

    ajout placeholder de la génération QR à la page confirmation (n'utilise pas les variables de session pour l'instant)

--- public/js/generation_qr.js
+++ public/js/generation_qr.js
@@ -1,9 +1,106 @@
 
-//Simplement appeler "generer()" avec comme parametre le contenant qui va générer le QR code équivalent à la valeur.
-//La valeur peut être feedée par PHP ou référé à une balise HTML contenant le code de l'image QR
+var codeQR ="";
 
-//NOTEZ BIEN; ceci n'est qu'une esquisse de la génération du code QR, et sera probablement optimisée selon les réels besoins en temps et lieu
-function generer(contenant, valeurQR)
+function generer()
 {
-    contenant.innerHTML = "<img src='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + valeurQR.innerHTML + "'>";
+    image = document.getElementById('image-code-qr');
+    image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + genererValeur();
+    
+    
+    
+
+}
+
+function genererValeur(){
+    if(codeQR === ""){
+        for(var i = 0; i < 7; i++)
+        {
+            let chiffreAleatoire = Math.floor(Math.random() * 26);
+            switch(chiffreAleatoire) {
+                case 1:
+                    codeQR += 'A';
+                break;
+                case 2:
+                    codeQR += 'B';
+                break;
+                case 3:
+                    codeQR += 'C';
+                break;
+                case 4:
+                    codeQR += 'D';
+                break;
+                case 5:
+                    codeQR += 'E';
+                break;
+                case 6:
+                    codeQR += 'F';
+                break;
+                case 7:
+                    codeQR += 'G';
+                break;
+                case 8:
+                    codeQR += 'H';
+                break;
+                case 9:
+                    codeQR += 'I';
+                break;
+                case 10:
+                    codeQR += 'J';
+                break;
+                case 11:
+                    codeQR += 'K';
+                break;
+                case 12:
+                    codeQR += 'L';
+                break;
+                case 13:
+                    codeQR += 'M';
+                break;
+                case 14:
+                    codeQR += 'N';
+                break;
+                case 15:
+                    codeQR += 'O';
+                break;
+                case 16:
+                    codeQR += 'P';
+                break;
+                case 17:
+                    codeQR += 'Q';
+                break;
+                case 18:
+                    codeQR += 'R';
+                break;
+                case 19:
+                    codeQR += 'S';
+                break;
+                case 20:
+                    codeQR += 'T';
+                break;
+                case 21:
+                    codeQR += 'U';
+                break;
+                case 22:
+                    codeQR += 'V';
+                break;
+                case 23:
+                    codeQR += 'W';
+                break;
+                case 24:
+                    codeQR += 'X';
+                break;
+                case 25:
+                    codeQR += 'Y';
+                break;
+                case 26:
+                    codeQR += 'Z';
+                break;
+            }
+        }
+    
+    
+        
+    }
+    //console.log(codeQR);
+    return codeQR;
 }

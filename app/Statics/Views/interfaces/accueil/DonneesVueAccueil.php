06621ca7ca84b06efd0cb239cef7560d311d7e58
BoHuny <BoHuny@hotmail.com>
2019-10-22 23:00:10 -0400

    Ajout 's' à 'Départs' page accueil

--- app/Statics/Views/interfaces/accueil/DonneesVueAccueil.php
+++ app/Statics/Views/interfaces/accueil/DonneesVueAccueil.php
@@ -13,7 +13,7 @@ class DonneesVueAccueil extends DonneesVue
         $this->nomVue = 'accueil';
 
         /* Les cles sont en snake_case */
-        $this->setDonneeVue('depart','Départ');
+        $this->setDonneeVue('depart','Départs');
         $this->setDonneeVue('choix_destination_heure_depart','Choisissez une destination/heure de départ');
         $this->setDonneeVue('choix_autre_date','Choisir une autre date');
         $this->setDonneeVue('choix_autre_depart','Choisir un autre départ');

09b616129e65b1b38b5e8be541915fd1fe32288c
Lucien Barcon <lucienbarcon@gmail.com>
2019-10-24 15:52:55 -0400

    Supression chaine magique sur reservation paiement

--- app/Statics/Views/interfaces/reservation_paiement/DonneesVueReservationPaiement.php
+++ app/Statics/Views/interfaces/reservation_paiement/DonneesVueReservationPaiement.php
@@ -10,11 +10,11 @@ class DonneesVueReservationPaiement extends DonneesVue
     {
         parent::__construct();
         $this->nomVue = 'reservation_paiement';
-        $this->setDonneeVue('renseigner_informations','Renseignez vos informations de paiement');
+        $this->setDonneeVue('renseigner_informations','Veuillez renseigner vos informations');
         $this->setDonneeVue('nom_prenom','Nom et prénom : ');
         $this->setDonneeVue('numero_carte','Numéro de carte : ');
         $this->setDonneeVue('code_securite','Code de sécurité : ');
         $this->setDonneeVue('payer','JE VALIDE MON PAIEMENT');
-
+        $this->setDonneeVue('payer','JE VALIDE MON PAIEMENT');
     }
 }

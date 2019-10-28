016df32f03994503f7a712f12e08537c87ccb41c
Antoine68 <antoine.richard68@gmail.com>
2019-10-28 16:54:03 -0400

    Ouverture du modal si la station a des dépendances

--- resources/views/interfaces/administration/components/head.blade.php
+++ resources/views/interfaces/administration/components/head.blade.php
@@ -23,6 +23,12 @@
 
     <script src="/resources/js/app.js"></script>
     <script src="/js/app.js"></script>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 
 
 

20d0c97ad6212d836a862da0223e33c11b30e851
Lucien Barcon <lucienbarcon@gmail.com>
2019-11-06 09:27:19 -0500

    Suppression des balises styles page validation

--- resources/views/interfaces/validation_informations/validation_informations.blade.php
+++ resources/views/interfaces/validation_informations/validation_informations.blade.php
@@ -1,15 +1,13 @@
 @extends('interfaces.index')
 
 @section('contenu')
-    <body id="top" style="background-color: #d3d3d3;">
-    <p class="h2 mb-4 font-weight-bold my-5 text-center"
-       style="color: midnightblue">{{ $validation_informations_recapitulatif_billet }}</p>
-
+    <body id="top">
+    <p class="h2 mb-4 font-weight-bold my-5 text-center" id="texteHaut">{{ $validation_informations_recapitulatif_billet }}</p>
 
     @include('interfaces.validation_informations.components.cadre_informations')
 
     <div class="container">
-        <a href="{{ route('reservation_paiement') }}" style="text-decoration: none">
+        <a href="{{ route('reservation_paiement') }}" id="textDecoration">
             <button type="button" class="btn btn-outline-success my-4 btn-block">
                 {{ $validation_informations_valider_informations_billet }}
             </button>

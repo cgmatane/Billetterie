53f9b2715020d9b1f9691dfef625846626255518
Lucien Barcon <lucienbarcon@gmail.com>
2019-11-07 14:16:57 -0500

    Suppression des balises styles page information

--- resources/views/interfaces/informations/informations.blade.php
+++ resources/views/interfaces/informations/informations.blade.php
@@ -2,11 +2,11 @@
 @extends('interfaces.index')
 
 @section('contenu')
-    <body id="top" style="background-color: #d3d3d3">
+    <body id="top">
     <div class="container-fluid">
         <div class="row mt-5 mb-5">
             <div class="col-12">
-                <h1 class="text-center font-weight-bold" style="color: midnightblue">
+                <h1 class="text-center font-weight-bold" id="texteHaut">
                     {{ $informations_titre }}
                 </h1>
             </div>
@@ -16,9 +16,9 @@
                 {!! $informations_contenu !!}
             </div>
             <div class="container-fluid" id="footerChoixMultiples">
-                <div style="width: 100% ;height: 300px; background-color: midnightblue; border-radius: 20px">
+                <div id="footerInformation">
                     <div class="row col-sm-12">
-                        <div class="col" id="zoneDeGauche">
+                        <div class="col" id="retourInformation">
                             @component('global_components.bouton_retour_precedent')
                                 Retour
                             @endcomponent
@@ -30,38 +30,4 @@
     </div>
 @endsection
 
-    <style>
-        @media screen and (min-width: 768px) {
-            #zoneDeDroite {
-                loat: right;
-                margin-right: 3%;
-                margin-top: 200px
-            }
 
-            #zoneDeGauche {
-                float: left;
-                margin-left: 5%;
-                margin-top: 200px;
-            }
-
-            #boutonRecommencer {
-                float: right;
-            }
-        }
-
-        @media screen and (max-width: 768px) {
-            #zoneDeDroite {
-                margin-top: 80px;
-                display: inline-block;
-                float: left;
-            }
-            #zoneDeGauche {
-                margin-top: 80px;
-                display: inline-block;
-                float: left;
-            }
-            #boutonRecommencer {
-                float: left;
-            }
-        }
-    </style>

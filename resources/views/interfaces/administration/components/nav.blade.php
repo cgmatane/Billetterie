b36e466c4d703d629738833e4103501a4e710a3b
Antoine68 <antoine.richard68@gmail.com>
2019-11-07 14:56:52 -0500

    Modification header administration

--- resources/views/interfaces/administration/components/nav.blade.php
+++ resources/views/interfaces/administration/components/nav.blade.php
@@ -1,4 +1,4 @@
 <nav class="nav-admin">
-    <div class="brand float-left"><img alt="logo-stq" src="/img/logo-stq-adm.png">@yield('titre')</div>
+    <div class="brand float-left"><img alt="logo-stq" src="/img/logo-stq.png">@yield('titre')</div>
     <div class="auth float-right">{{ $email }}</div>
 </nav>

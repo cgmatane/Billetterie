cbee9d9c8b16fb60ed91fc195be7526245efc298
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-11-13 15:34:21 -0500

    Fermeture d'une balise div non fermée

--- resources/views/interfaces/accueil/accueil.blade.php
+++ resources/views/interfaces/accueil/accueil.blade.php
@@ -1,115 +1,114 @@
 @extends('interfaces.index')
 
 @section('contenu')
-<div class="container-fluid" id="imageFond">
-    <div>
-    <div class="text-center row justify-content-center" id="noMargin">
-        <div class="col-sm-6 col-xs-8 shadow-lg rounded-lg  mt-6" id="midnightBlue">
-            @component('interfaces.accueil.components.titre_principal')
-                @if($validation != "")
-                    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
-                    <noscript>{{$global_activer_javascript}}</noscript>
-                    <script>
-                        Swal.fire(
-                            'Commande validée !',
-                            'Un mail vous a été envoyé.',
-                            'success'
-                        )
-                    </script>
-                    <noscript>{{$global_activer_javascript}}</noscript>
-                @endif
+    <div class="container-fluid" id="imageFond">
+        <div class="text-center row justify-content-center" id="noMargin">
+            <div class="col-sm-6 col-xs-8 shadow-lg rounded-lg  mt-6" id="midnightBlue">
+                @component('interfaces.accueil.components.titre_principal')
+                    @if($validation != "")
+                        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+                        <noscript>{{$global_activer_javascript}}</noscript>
+                        <script>
+                            Swal.fire(
+                                'Commande validée !',
+                                'Un mail vous a été envoyé.',
+                                'success'
+                            )
+                        </script>
+                        <noscript>{{$global_activer_javascript}}</noscript>
+                    @endif
 
-                {{ $accueil_depart }}
-                @if(empty($depart))
-                    Matane
-                @else
-                    {{ $depart }}
-                @endif
-                @if(empty($date))
-                    4 Septembre
-                @else
-                    {{ $date }}
-                @endif
-            @endcomponent
+                    {{ $accueil_depart }}
+                    @if(empty($depart))
+                        Matane
+                    @else
+                        {{ $depart }}
+                    @endif
+                    @if(empty($date))
+                        4 Septembre
+                    @else
+                        {{ $date }}
+                    @endif
+                @endcomponent
+            </div>
         </div>
-    </div>
 
-    <div class="text-center row justify-content-center">
-        <div class="bg-white col-sm-6 col-xs-8 bg-light pb-5 shadow-lg rounded-lg">
-            @component('interfaces.accueil.components.titre_choix_destination_heure_depart')
-                {{ $accueil_choix_destination_heure_depart }}
-            @endcomponent
+        <div class="text-center row justify-content-center">
+            <div class="bg-white col-sm-6 col-xs-8 bg-light pb-5 shadow-lg rounded-lg">
+                @component('interfaces.accueil.components.titre_choix_destination_heure_depart')
+                    {{ $accueil_choix_destination_heure_depart }}
+                @endcomponent
 
-            @isset($trajets)
-                @foreach ($trajets as $trajet)
-                    @component('interfaces.accueil.components.bouton_choix_trajet')
-                        @slot('id') {{ $trajet['id'] }} @endslot
-                        @slot('destination') {{ $trajet['stationArrivee'] }} @endslot
-                        @slot('heure') {{ $trajet['heureDepart'] }} @endslot
+                @isset($trajets)
+                    @foreach ($trajets as $trajet)
+                        @component('interfaces.accueil.components.bouton_choix_trajet')
+                            @slot('id') {{ $trajet['id'] }} @endslot
+                            @slot('destination') {{ $trajet['stationArrivee'] }} @endslot
+                            @slot('heure') {{ $trajet['heureDepart'] }} @endslot
+                        @endcomponent
+                    @endforeach
+                @endisset
+
+
+                @component('interfaces.accueil.components.bouton_choix_trajet')
+                    @slot('id') 1 @endslot
+                    @slot('destination') Godbout @endslot
+                    @slot('heure') 10h00 @endslot
+                @endcomponent
+
+                @component('interfaces.accueil.components.bouton_choix_trajet')
+                    @slot('id') 1 @endslot
+                    @slot('destination') Godbout @endslot
+                    @slot('heure') 13h30 @endslot
+                @endcomponent
+
+                @component('interfaces.accueil.components.bouton_choix_trajet')
+                    @slot('id') 1 @endslot
+                    @slot('destination') Godbout @endslot
+                    @slot('heure') 16h00 @endslot
+                @endcomponent
+
+                @component('interfaces.accueil.components.bouton_choix_trajet')
+                    @slot('id') 1 @endslot
+                    @slot('destination') Baie-Comeau @endslot
+                    @slot('heure') 10h45 @endslot
+                @endcomponent
+
+                @component('interfaces.accueil.components.bouton_choix_trajet')
+                    @slot('id') 1 @endslot
+                    @slot('destination') Baie-Comeau @endslot
+                    @slot('heure') 13h30 @endslot
+                @endcomponent
+
+                @component('interfaces.accueil.components.bouton_choix_trajet')
+                    @slot('id') 1 @endslot
+                    @slot('destination') Baie-Comeau @endslot
+                    @slot('heure') 15h30 @endslot
+                @endcomponent
+
+            </div>
+        </div>
+        <div class="text-center row justify-content-center">
+            <div class="col-sm-6 col-xs-8 p-0">
+                <div class="col-sm-5 col-xs-12 float-left">
+                    @component('interfaces.accueil.components.bouton_changer_parametres_depart')
+                        @slot('route')
+                            {{ route('choix_date') }}
+                        @endslot
+                        {{ $accueil_choix_autre_date }}
                     @endcomponent
-                @endforeach
-            @endisset
+                </div>
 
-
-            @component('interfaces.accueil.components.bouton_choix_trajet')
-                @slot('id') 1 @endslot
-                @slot('destination') Godbout @endslot
-                @slot('heure') 10h00 @endslot
-            @endcomponent
-
-            @component('interfaces.accueil.components.bouton_choix_trajet')
-                @slot('id') 1 @endslot
-                @slot('destination') Godbout @endslot
-                @slot('heure') 13h30 @endslot
-            @endcomponent
-
-            @component('interfaces.accueil.components.bouton_choix_trajet')
-                @slot('id') 1 @endslot
-                @slot('destination') Godbout @endslot
-                @slot('heure') 16h00 @endslot
-            @endcomponent
-
-            @component('interfaces.accueil.components.bouton_choix_trajet')
-                @slot('id') 1 @endslot
-                @slot('destination') Baie-Comeau @endslot
-                @slot('heure') 10h45 @endslot
-            @endcomponent
-
-            @component('interfaces.accueil.components.bouton_choix_trajet')
-                @slot('id') 1 @endslot
-                @slot('destination') Baie-Comeau @endslot
-                @slot('heure') 13h30 @endslot
-            @endcomponent
-
-            @component('interfaces.accueil.components.bouton_choix_trajet')
-                @slot('id') 1 @endslot
-                @slot('destination') Baie-Comeau @endslot
-                @slot('heure') 15h30 @endslot
-            @endcomponent
-
-        </div>
-    </div>
-    <div class="text-center row justify-content-center">
-        <div class="col-sm-6 col-xs-8 p-0">
-            <div class="col-sm-5 col-xs-12 float-left">
-        @component('interfaces.accueil.components.bouton_changer_parametres_depart')
-            @slot('route')
-                {{ route('choix_date') }}
-            @endslot
-            {{ $accueil_choix_autre_date }}
-        @endcomponent
-            </div>
-
-            <div class="col-sm-5 col-xs-12 float-right">
-        @component('interfaces.accueil.components.bouton_changer_parametres_depart')
-            @slot('route')
-                {{ route('choix_depart') }}
-            @endslot
-            {{ $accueil_choix_autre_depart }}
-        @endcomponent
+                <div class="col-sm-5 col-xs-12 float-right">
+                    @component('interfaces.accueil.components.bouton_changer_parametres_depart')
+                        @slot('route')
+                            {{ route('choix_depart') }}
+                        @endslot
+                        {{ $accueil_choix_autre_depart }}
+                    @endcomponent
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection

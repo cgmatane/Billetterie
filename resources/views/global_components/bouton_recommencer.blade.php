2c461cbb9ea041d71abb3b388c8ef3ce6069a2d9
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-11-13 15:50:33 -0500

    Correction HTML

--- resources/views/global_components/bouton_recommencer.blade.php
+++ resources/views/global_components/bouton_recommencer.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('index') }}">
-    <button id="boutonRecommencer" type="button" class="btn btn-outline-retour-menu p-3 mb-5">
+    <div id="boutonRecommencer" class="btn btn-outline-retour-menu p-3 mb-5">
         <em class="fas fa-undo-alt"></em>
         {{ $slot }}
-    </button>
+    </div>
 </a>

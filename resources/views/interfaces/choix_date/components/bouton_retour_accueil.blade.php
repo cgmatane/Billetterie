90ee1a4b9311d4b6efff005ec24f18115d3b34da
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-11-13 16:03:07 -0500

    HTML choix date

--- resources/views/interfaces/choix_date/components/bouton_retour_accueil.blade.php
+++ resources/views/interfaces/choix_date/components/bouton_retour_accueil.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ $route }}">
-    <button type="button" class="btn btn-outline-param mt-5 p-3 mb-5" style="font-size:1.5em">
+    <div class="btn btn-outline-param mt-5 p-3 mb-5" style="font-size:1.5em">
         {{ $slot }}
-    </button>
+    </div>
 </a>

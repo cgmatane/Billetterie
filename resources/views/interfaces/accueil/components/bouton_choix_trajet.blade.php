bb2d90bfde988fc7098f76e4ae071bee2d74ed59
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-11-18 15:24:46 -0500

    style focus clavier page d'accueil

--- resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
+++ resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
@@ -1,10 +1,8 @@
-<a href="?idTrajet={{ $id }}">
-    <div class="btn btn-outline-success mt-3 col-11" >
-        <div class="display-4" style="text-transform: uppercase">
-            <p class = "mt-1 mb-1">
-                <em class="fas fa-map-marker-alt" style="color:blue"></em> {{ $destination }}
-                <em class="far fa-clock" style="color:tomato"></em> {{ $heure }}
-            </p>
-        </div>
+<a href="?idTrajet={{ $id }}" class="btn btn-outline-success mt-3 col-11">
+    <div class="display-4" style="text-transform: uppercase">
+        <p class="mt-1 mb-1">
+            <em class="fas fa-map-marker-alt" style="color:blue"></em> {{ $destination }}
+            <em class="far fa-clock" style="color:tomato"></em> {{ $heure }}
+        </p>
     </div>
 </a>

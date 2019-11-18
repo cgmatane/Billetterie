6ee2d0fcbeb53e03000231b03474b65cf84afff1
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-11-18 15:34:25 -0500

    style focus clavier page choix départ

--- resources/views/interfaces/choix_depart/components/bouton_liste_options.blade.php
+++ resources/views/interfaces/choix_depart/components/bouton_liste_options.blade.php
@@ -1,8 +1,6 @@
-<a href="?{{ $typeInformation }}={{ $contenu }}">
-    <div class="btn btn-outline-success mt-3 col-11 p-3" >
-        <div class="display-4">
-            <p class="mt-1 mb-1">{{ $contenu }}</p>
-        </div>
+<a class="btn btn-outline-success mt-3 col-11 p-3" href="?{{ $typeInformation }}={{ $contenu }}">
+    <div class="display-4">
+        <p class="mt-1 mb-1">{{ $contenu }}</p>
     </div>
 </a>
 <br>
--- resources/views/interfaces/choix_depart/components/bouton_retour_accueil.blade.php
+++ resources/views/interfaces/choix_depart/components/bouton_retour_accueil.blade.php
@@ -1,5 +1,3 @@
-<a href="{{ $route }}">
-    <div class="btn btn-outline-param mt-5 p-3 mb-5" style="font-size:1.5em">
-        {{ $slot }}
-    </div>
+<a class="btn btn-outline-param mt-5 p-3 mb-5" href="{{ $route }}" style="font-size: 1.5em">
+    {{ $slot }}
 </a>

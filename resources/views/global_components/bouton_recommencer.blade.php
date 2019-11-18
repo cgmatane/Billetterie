638e16d3daff757313b4be2ed9e4b62e73344adb
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-11-18 15:50:47 -0500

    style focus clavier boutons recommencer et précedent

--- resources/views/global_components/bouton_recommencer.blade.php
+++ resources/views/global_components/bouton_recommencer.blade.php
@@ -1,6 +1,5 @@
-<a href="{{ route('index') }}" style="pointer-events: initial">
-    <div id="boutonRecommencer" class="btn btn-outline-retour-menu p-3 mb-5">
-        <em class="fas fa-undo-alt"></em>
-        {{ $slot }}
-    </div>
+<a id="boutonRecommencer" class="btn btn-outline-retour-menu p-3 mb-5" href="{{ route('index') }}"
+   style="pointer-events: initial">
+    <em class="fas fa-undo-alt"></em>
+    {{ $slot }}
 </a>
--- resources/views/global_components/bouton_retour_precedent.blade.php
+++ resources/views/global_components/bouton_retour_precedent.blade.php
@@ -1,5 +1,4 @@
-<a href="javascript:history.go(-1)" style="pointer-events: initial">
-    <div class="btn btn-outline-retour p-3"> <em class="fas fa-arrow-circle-left"></em>
-        {{ $slot }}
-    </div>
+<a class="btn btn-outline-retour p-3" href="javascript:history.go(-1)" style="pointer-events: initial">
+    <em class="fas fa-arrow-circle-left"></em>
+    {{ $slot }}
 </a>

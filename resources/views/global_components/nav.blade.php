99bcf4e32dab5cbf672cbfde66db43438692b358
Loic Hug <h-loic@orange.fr>
2019-11-18 17:33:07 -0500

    bouton changer de langue dans la navblyat

--- resources/views/global_components/nav.blade.php
+++ resources/views/global_components/nav.blade.php
@@ -2,17 +2,28 @@
     <div class="container-fluid">
         <header class="blog-header py-3">
             <div class="row justify-content-between align-items-center">
-                <div class="col-sm-12 col-md-4 pt-1">
+                <div class="col-md-12 col-lg-4 pt-1">
                     <a href="{{ route('index') }}"> <!-- Route à définir -->
                         <img class="mx-auto pl-5" src="/img/logo-stq.png" alt="Logo de la STQ">
                     </a>
                 </div>
-                <div class="col-sm-12 col-md-4 text-center">
+                <div class="col-md-12 col-lg-4 text-center">
                     <h1 class="text-center text-dark text-dark">{{ $global_traverse }}</h1>
                 </div>
 
-                <div class="col-sm-12 col-md-4 d-flex align-items-center text-dark ">
+                <div class="col-md-12 col-lg-2 align-items-center text-dark ">
                     <a class="mx-auto btn btn-lg btn-outline-param" href="{{ route('informations') }}">{{ $global_informations }}</a> <!-- Route à définir -->
+                </div>
+                <div class="dropdown col-md-12 col-lg-2 align-items-center text-dark">
+                    <button class="btn btn-outline-param dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Changer de langue
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="{{ route('changer_langue') }}?langue=0&provenance={{ Request::url() }}">
+                            Francais</a>
+                        <a class="dropdown-item" href="{{ route('changer_langue') }}?langue=1&provenance={{ Request::url() }}">
+                            English</a>
+                    </div>
                 </div>
             </div>
             <!--
@@ -21,17 +32,6 @@
                 <a href="{{ route('changer_langue') }}?langue=1&provenance={{ Request::url() }}"> EN </a>
             </div>
             -->
-            <div class="dropdown">
-                <button class="btn btn-outline-param dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Changer de langue
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="{{ route('changer_langue') }}?langue=0&provenance={{ Request::url() }}">
-                        Francais</a>
-                    <a class="dropdown-item" href="{{ route('changer_langue') }}?langue=1&provenance={{ Request::url() }}">
-                        English</a>
-                </div>
-            </div>
         </header>
     </div>
 </nav>

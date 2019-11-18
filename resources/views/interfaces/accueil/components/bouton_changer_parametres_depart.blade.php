698ee2400659db7415b94ea6d6ba226d8ef1e819
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-11-18 15:04:59 -0500

    style focus clavier pour faciliter la lecture nav

--- resources/views/interfaces/accueil/components/bouton_changer_parametres_depart.blade.php
+++ resources/views/interfaces/accueil/components/bouton_changer_parametres_depart.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ $route }}">
+<a class="btn-outline-param" href="{{ $route }}">
     <div class="btn btn-outline-param my-5 p-3" style="font-size: 1.5em">
         {{ $slot }}
     </div>

7d13f180759b66067da79fb6d44e639592b8463f
Benjamin Boullé <1934904@cgmatane.qc.ca>
2019-11-17 19:39:54 -0500

    Retrait log sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -4,7 +4,6 @@ function myFunction() {
     var navSide = document.getElementsByClassName("nav-side-menu")[0];
     var x = document.body.scrollTop || document.documentElement.scrollTop;
 
-    console.log(x);
 
 
     if (x < 55){

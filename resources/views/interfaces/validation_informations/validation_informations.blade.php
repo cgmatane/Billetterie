0555042e5ca8b2014c4facee8be3ec9e5fbf7547
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-11-18 15:56:03 -0500

    style focus clavier valider informations

--- resources/views/interfaces/validation_informations/validation_informations.blade.php
+++ resources/views/interfaces/validation_informations/validation_informations.blade.php
@@ -3,15 +3,16 @@
 @section('titre', $validation_informations_titre_page)
 
 @section('contenu')
-    <p class="h2 mb-4 font-weight-bold my-5 text-center" id="texteHaut">{{ $validation_informations_recapitulatif_billet }}</p>
+    <p class="h2 mb-4 font-weight-bold my-5 text-center"
+       id="texteHaut">{{ $validation_informations_recapitulatif_billet }}</p>
 
     @include('interfaces.validation_informations.components.cadre_informations')
 
     <div class="container">
-        <a href="{{ route('reservation_paiement') }}" id="textDecoration">
-            <div class="btn btn-outline-success my-4 btn-block">
-                {{ $validation_informations_valider_informations_billet }}
-            </div>
+        <a href="{{ route('reservation_paiement') }}" id="textDecoration"
+           class="btn btn-outline-success my-4 btn-block">
+            {{ $validation_informations_valider_informations_billet }}
+
         </a>
     </div>
     <div class="container-fluid">

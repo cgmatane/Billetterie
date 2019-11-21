58f59574052285ab3997b0c11aee8d3bbaee5b9e
Antoine68 <antoine.richard68@gmail.com>
2019-11-21 16:08:45 -0500

    Test si le trajet n'est pas annulé vue generale administration

--- resources/views/interfaces/connexion/connexion.blade.php
+++ resources/views/interfaces/connexion/connexion.blade.php
@@ -15,6 +15,7 @@
         @endif
         @if(count($errors) > 0)
             <div class="alert alert-danger">
+                <button type="button" class="close" data-dismiss="alert">x</button>
                 <ul>
                     @foreach($errors->all() as $error)
                         <li>{{ $error }}</li>

c65c3df8107010766fa711b692bfb6ff607e2072
Loic Hug <h-loic@orange.fr>
2019-11-24 19:49:20 -0500

    ouverture d'un modal pour chaque boutons informations sur le trajet

--- resources/views/interfaces/accueil/accueil.blade.php
+++ resources/views/interfaces/accueil/accueil.blade.php
@@ -2,6 +2,10 @@
 
 @section('titre', $global_titre)
 
+<script>
+    var i=0;
+</script>
+
 @section('contenu')
     <div class="container-fluid" id="imageFond">
         <div class="text-center row justify-content-center" id="noMargin">
@@ -56,46 +60,6 @@
         </script>
         <noscript>{{ $global_activer_javascript }}</noscript>
     @endisset
-
-
-    <div id="myModal" class="modal">
-
-        <!-- Modal content -->
-        <div class="modal-content">
-            <span class="close">&times;</span>
-            <p>Some text in the Modal..</p>
-        </div>
-
-    </div>
-
-    <script>
-        // Get the modal
-        var modal = document.getElementById("myModal");
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on the button, open the modal
-        btn.onclick = function() {
-            console.log("wfuigwigwfuig");
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    </script>
 
 @endsection
 
--- resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
+++ resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
@@ -2,12 +2,60 @@
 <a href="?idTrajet={{ $id }}" class="btn btn-outline-success mt-3 col-11">
     <div class="display-4" style="text-transform: uppercase">
         <p class="mt-1 mb-1">
-            <em class="fas fa-map-marker-alt" style="color:blue"></em> {{ $destination }}
-            <em class="far fa-clock" style="color:tomato"></em> {{ $heure }}
+            <em class="fas fa-map-marker-alt" style="color:blue"></em><span id="destination">{{ $destination }}</span>
+            <em class="far fa-clock" style="color:tomato"></em><span id="heure"> {{ $heure }}</span>
         </p>
     </div>
 </a>
-    <div id="myBtn" class="align-middle d-inline-block">
+    <div class="align-middle d-inline-block idInformation">
         <i class="fa fa-info-circle float-right fa-2x" style="color:blue"></i>
     </div>
 </div>
+
+
+<div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Some text in the Modal..</p>
+    </div>
+
+</div>
+
+<script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+
+    var idInformation =document.getElementsByClassName("idInformation")[i];
+    idInformation.setAttribute("id", i);
+
+    var btn = document.getElementById(i);
+    console.log(i);
+
+    i++;
+
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        console.log("wfuigwigwfuig");
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>

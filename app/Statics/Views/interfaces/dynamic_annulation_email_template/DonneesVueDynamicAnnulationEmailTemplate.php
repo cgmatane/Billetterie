946d25c7f490146fd9bcdf2c36a518c31453cb01
Benjamin Boullé <1934904@cgmatane.qc.ca>
2019-11-26 18:14:20 -0500

    Correction traduction mail annulation

--- app/Statics/Views/interfaces/dynamic_annulation_email_template/DonneesVueDynamicAnnulationEmailTemplate.php
+++ app/Statics/Views/interfaces/dynamic_annulation_email_template/DonneesVueDynamicAnnulationEmailTemplate.php
@@ -14,9 +14,9 @@ class DonneesVueDynamicAnnulationEmailTemplate extends DonneesVue
         $this->setDonneeVue('annonce_annulation',['La Société des Traversiers Québec est dans le regret de vous annoncer que la traverse qui devait partir de ', 'The STQ is sorry to announce that the ferry that was to leave from']);
         $this->setDonneeVue('le',['le','on']);
         $this->setDonneeVue('a',['à','at']);
-        $this->setDonneeVue('arriver',['et arriver', 'and arrive']);
-        $this->setDonneeVue('explication',['a été annulé et n\'aura donc pas lieu.', 'has been canceled and will not take place.']);
-        $this->setDonneeVue('remboursement',['Vous allez être remboursés de la totalité du montant du ticket dans les jours à venir.', 'You will be refunded the full amount of the ticket in the coming days.']);
+        $this->setDonneeVue('arriver',['et arriver', 'and arrives']);
+        $this->setDonneeVue('explication',['a été annulé et n\'aura donc pas lieu.', 'has been cancelled and thus won\'t take place.']);
+        $this->setDonneeVue('remboursement',['Vous allez être remboursé de la totalité du montant du ticket dans les jours à venir.', 'You will be refunded by the full amount of the ticket in the upcoming days.']);
         $this->setDonneeVue('comprehension',['Merci de votre compréhension.', 'Thank you for your understanding.']);
     }
 }

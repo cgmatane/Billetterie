6cb90eb3e4e40474c2a1c3892d5db3ec5c52041a
Loic Hug <h-loic@orange.fr>
2019-11-25 19:15:52 -0500

    renomage du css pour le modal informations

--- resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
+++ resources/views/interfaces/accueil/components/bouton_choix_trajet.blade.php
@@ -10,7 +10,45 @@
             </p>
         </div>
     </a>
-    <div id="myBtn" class="align-middle d-inline-block">
+    <div id="myBtn" class="align-middle d-inline-block idInformation">
         <i class="fa fa-info-circle float-right fa-2x" style="color:blue"></i>
     </div>
 </div>
+
+<div id="myModal" class="modal2">
+
+    <!-- Modal content -->
+    <div class="modal-content2">
+        <span class="close2">&times;</span>
+        <p>Some text in the Modal..</p>
+    </div>
+
+</div>
+
+<script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    // Get the button that opens the modal
+    var idInformation =document.getElementsByClassName("idInformation")[i];
+    idInformation.setAttribute("id", i);
+    var btn = document.getElementById(i);
+    console.log(i);
+    i++;
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close2")[0];
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        console.log("wfuigwigwfuig");
+        modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>

25a43dde1b459e96033701d0ad1d5ede452aa179
Antoine68 <antoine.richard68@gmail.com>
2019-11-26 16:50:06 -0500

    (Google)Traduction du mail d'annulation

--- app/Statics/Views/interfaces/dynamic_annulation_email_template/DonneesVueDynamicAnnulationEmailTemplate.php
+++ app/Statics/Views/interfaces/dynamic_annulation_email_template/DonneesVueDynamicAnnulationEmailTemplate.php
@@ -11,12 +11,12 @@ class DonneesVueDynamicAnnulationEmailTemplate extends DonneesVue
         parent::__construct($langue);
         $this->nomVue = 'dynamic_annulation_email_template';
         $this->setDonneeVue('bonjour',['Bonjour', 'Hello']);
-        $this->setDonneeVue('annonce_annulation',['La Société des Traversiers Québec est dans le regret de vous annoncer que la traverse qui devait partir de ', 'The STQ thanks you for your purchase']);
+        $this->setDonneeVue('annonce_annulation',['La Société des Traversiers Québec est dans le regret de vous annoncer que la traverse qui devait partir de ', 'The STQ is sorry to announce that the ferry that was to leave from']);
         $this->setDonneeVue('le',['le','on']);
         $this->setDonneeVue('a',['à','at']);
-        $this->setDonneeVue('arriver',['et arriver', 'Your trip will take place on']);
-        $this->setDonneeVue('explication',['à été annulé et n\'aura donc pas lieu.', 'Download your ticket in the attachment']);
-        $this->setDonneeVue('remboursement',['Vous allez être remboursés de la totalité du montant du ticket dans les jours à venir.', 'Don\'t forget to be present in the boarding 15 minutes before the hour displayed on your ticket.']);
-        $this->setDonneeVue('comprehension',['Merci de votre compréhension.', 'Don\'t forget to be present in the boarding 15 minutes before the hour displayed on your ticket.']);
+        $this->setDonneeVue('arriver',['et arriver', 'and arrive']);
+        $this->setDonneeVue('explication',['a été annulé et n\'aura donc pas lieu.', 'has been canceled and will not take place.']);
+        $this->setDonneeVue('remboursement',['Vous allez être remboursés de la totalité du montant du ticket dans les jours à venir.', 'You will be refunded the full amount of the ticket in the coming days.']);
+        $this->setDonneeVue('comprehension',['Merci de votre compréhension.', 'Thank you for your understanding.']);
     }
 }

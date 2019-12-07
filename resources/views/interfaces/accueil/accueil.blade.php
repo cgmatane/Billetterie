3d239cf875468f3d551e0c768ee64fa65a0bdfc2
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-12-07 16:11:02 -0500

    changement niveau de titre

--- resources/views/interfaces/accueil/accueil.blade.php
+++ resources/views/interfaces/accueil/accueil.blade.php
@@ -57,7 +57,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">{{ $accueil_infos_trajet }}</h5>
+                    <h4 class="modal-title" id="exampleModalLongTitle">{{ $accueil_infos_trajet }}</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>

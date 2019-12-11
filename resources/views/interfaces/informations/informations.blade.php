1ae71469b4ebf05b4dad57f38bae163d645687d7
Lucien Barcon <lucienbarcon@gmail.com>
2019-12-11 08:51:43 -0500

    Sixième question FAQ

--- resources/views/interfaces/informations/informations.blade.php
+++ resources/views/interfaces/informations/informations.blade.php
@@ -37,6 +37,10 @@
                             <p class="font-weight-bold">{{ $informations_question_traverse }}</p>
                             <p>{{ $informations_reponse_traverse }}</p>
                         </li>
+                        <li>
+                            <p class="font-weight-bold">{{ $informations_question_transaction }}</p>
+                            <p>{{ $informations_reponse_transaction }}</p>
+                        </li>
                     </ul>
                 </div>
             </div>
